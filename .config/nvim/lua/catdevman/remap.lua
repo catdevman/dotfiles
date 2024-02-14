9342e3fb118b2cdb698eaba9a1db2df54b78d660
Lucas Pearson <catdevman@gmail.com>
2024-02-14 14:33:30 -0500

    moving everything around to work with stow

--- .config/nvim/lua/catdevman/remap.lua
+++ .config/nvim/lua/catdevman/remap.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>t", vim.cmd.Ex)
+vim.keymap.set("n", "<leader><CR>", ":so ~/.config/nvim/init.lua<CR>")
+
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
