549dbea6776bcf1374a6ba81a7785a43d9dc20c2
Lucas Pearson <lpearson@amplifiloyalty.com>
2024-04-04 07:10:27 -0400

    lookin good

---
+++ .config/nvim/after/plugin/tmux-navigator.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<C-S-h>", ":TmuxNavigateLeft<cr>")
+vim.keymap.set("n", "<C-S-j>", ":TmuxNavigateDown<cr>")
+vim.keymap.set("n", "<C-S-k>", ":TmuxNavigateUp<cr>")
+vim.keymap.set("n", "<C-S-l>", ":TmuxNavigateRight<cr>")
